63b5bdf8bc6187b61eb7bbd74a2b6424131cafdb
VladimirMosjak <vladimir.mosjak@gmail.com>
2025-10-04 12:54:31 +0200

    Create __manifest__.py

---
+++ addons/dm_partner_enrichment/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "DM Partner Enrichment",
+    "summary": "Pole pro Kontakty: web, právní stav, jednatel, zdroj apod.",
+    "version": "17.0.1.0.0",
+    "author": "Vladimír Mosjak",
+    "license": "LGPL-3",
+    "depends": ["base", "contacts"],
+    "data": ["views/res_partner_view.xml"],
+    "installable": True,
+    "application": False
+}
